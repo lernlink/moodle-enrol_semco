b56f18a20ddc7942d9935d7578a2645f9509fba1
Alexander Bias <alexander.bias@lernlink.de>
2024-06-18 07:47:34 +0200

    Release v4.1-r5

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_semco';
-$plugin->version = 2022112813;
-$plugin->release = 'v4.1-r4';
+$plugin->version = 2022112814;
+$plugin->release = 'v4.1-r5';
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 401];
 $plugin->maturity = MATURITY_STABLE;
